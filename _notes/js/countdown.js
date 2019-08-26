3d7825fb95d9b9e0d6846a587e434940709af6b5
Stephen Houser <stephenhouser@gmail.com>
2019-08-26 13:21:59 -0400

    update activity in module 2

--- _notes/js/countdown.js
+++ _notes/js/countdown.js
@@ -1,4 +1,4 @@
-function startTimer(duration, display) {
+function startTimer(duration, warningTime, display) {
 	var timer = duration, minutes, seconds;
 	interval = setInterval(function () {
 		minutes = parseInt(timer / 60, 10);
@@ -14,7 +14,7 @@ function startTimer(duration, display) {
 			clearInterval(interval);
 			display.classList.remove('timer-warning')
 			display.classList.add('timer-done')
-		} else if (timer < 10) {
+		} else if (timer < warning) {
 			display.classList.add('timer-warning')
 		}
 	}, 1000);
@@ -23,7 +23,7 @@ function startTimer(duration, display) {
 }
 
 var timer = null;
-function twoMinutes(timespan) {
+function countdownTimer(timespan, countdownTime, warningTime) {
 	var display = document.querySelector(timespan);
 	if (timer != null) {
 		clearInterval(timer);
@@ -32,11 +32,19 @@ function twoMinutes(timespan) {
 		display.classList.remove('timer-warning')
 		display.classList.remove('timer-done')
 	} else {
-		var twoMinutes = 60 * 2;
-		timer = startTimer(twoMinutes, display);
+		timer = startTimer(countdownTime, warningTime, display);
 		display.classList.remove('timer-warning')
 		display.classList.remove('timer-done')
 		display.classList.add('timer-active');
 	}
 
 };
+
+function twoMinutes(timespan) {
+	countdownTimer(timespan, (60 * 2), 10);
+};
+
+function fifteenMinutes(timespan) {
+	countdownTimer(timespan, (60 * 15), 60);
+};
+
---
+++ _posts/2019-10-30-10-presentation-multimedia.md
@@ -0,0 +1,8 @@
+---
+title: 10 - Presentation and Multimedia
+permalink: /10-presentation-multimedia
+---
+
+<!--more-->
+
+[7 - Presentation Slides][/assets/7-Presentation.pdf]
